fb63188924492bd69852648edc8ad00600f05dcc
PlatGod <an.charlie@gmail.com>
2017-03-23 22:18:37 -0400

    main method basically complete

--- Code/Tremaux.py
+++ Code/Tremaux.py
@@ -2,10 +2,19 @@ def main(past_moves,steps):
     inmaze=True;
     while inmaze=True:
         junction = isAtJunction()
-
-
+        if junction==True:
+            marked = isPathMarked()
+            if marked==True:
+                sub_alg(steps)
+            else:
+                #go backwards
+        else:
+            sub_alg(steps)
+        steps+=1
+    
 
 
 
 def isAtJunction():
-def sub_alg():
+def isPathMarked():
+def sub_alg(steps):
