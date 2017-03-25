636d577870177e8808d01f718dfdce4f00981b29
PlatGod <an.charlie@gmail.com>
2017-03-25 17:45:31 -0400

    finished tremaux code, just need to add code for sensors so we can turn

--- Code/Tremaux.py
+++ Code/Tremaux.py
@@ -12,9 +12,36 @@ def main(past_moves,steps):
             sub_alg(steps)
         steps+=1
     
-
-
-
 def isAtJunction():
 def isPathMarked():
+def canGoForward():
+def canGoRight():
+def canGoLeft():
 def sub_alg(steps):
+    forward=canGoForward()
+    if forward==True:
+        #moving forward
+        past_moves.append("forward")
+        main(past_moves,steps)
+    else:
+        right=canGoRight()
+        if right==True:
+            #turn right
+            past_moves.append("right")
+            main(past_moves,steps)
+        else:
+            left=canGoLeft()
+            if left==True:
+                #turn left
+                past_moves.append("left")
+                main(past_moves,steps)
+            else:
+                #move backwards code here
+                past_moves.append("backwards")
+                main(past_moves,steps)
+                
+
+
+steps=0
+past_moves=[]
+main(past_moves,steps)
