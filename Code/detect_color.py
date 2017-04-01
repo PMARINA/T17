13d3e3dd7e101b516ea1aab54547e15046e4ca1f
PlatGod <an.charlie@gmail.com>
2017-03-31 23:19:42 -0400

    ff

--- Code/detect_color.py
+++ Code/detect_color.py
@@ -7,27 +7,30 @@ import imutils
 import cv2
 
 class detect_color:
-    def __init__(self,image):
+    def __init__(self,inputimage):
         self.inputimage=image
         self.image = cv2.imread(inputimage)
         self.boundaries = [([220, 220, 220], [255, 255, 255])]
         for (lower, upper) in boundaries:
             lower = np.array(lower, dtype = "uint8")
             upper = np.array(upper, dtype = "uint8")
-            mask = cv2.inRange(image, lower, upper)
-            output = cv2.bitwise_and(image, image, mask=mask)
+            mask = cv2.inRange(self.image, lower, upper)
+            self.output = cv2.bitwise_and(self.image, self.image, mask=mask)
         
-    def isFlame(self,image, black_max_bgr=(40, 40, 40)):
+    def isFlame(self, black_max_bgr=(40, 40, 40)):
         # use this if you want to check channels are all basically equal
         # I split this up into small steps to find out where your error is coming from
-        mean_bgr_float = np.mean(image, axis=(0,1))
+        mean_bgr_float = np.mean(self.output, axis=(0,1))
         mean_bgr_rounded = np.round(mean_bgr_float)
         mean_bgr = mean_bgr_rounded.astype(np.uint8)
         # use this if you just want a simple threshold for simple grayscale
         # or if you want to use an HSV (V) measurement as in your example
-        mean_intensity = int(round(np.mean(image)))
+        mean_intensity = int(round(np.mean(self.output)))
         return False if np.all(mean_bgr < black_max_bgr) else True        
 
+    def leftright(self):
+        coord = np.where(np.all(self.output == (255, 255, 255), axis=-1))
+        print zip(coord[0], coord[1])
 
 # construct the argument parse and parse the arguments
 #ap = argparse.ArgumentParser()
